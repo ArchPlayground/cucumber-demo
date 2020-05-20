f1a757a3721762abc3cdc6ebe76b737a293a6a4c
RahulV <rverma@upchain.com>
2020-05-19 23:03:40 -0400

    :fire: Cleanup

--- src/test/java/com/upchain/arch/cucumberdemo/bdd/CucumberTestContextConfiguration.java
+++ src/test/java/com/upchain/arch/cucumberdemo/bdd/CucumberTestContextConfiguration.java
@@ -2,7 +2,6 @@ package com.upchain.arch.cucumberdemo.bdd;
 
 import io.cucumber.java.Before;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import com.upchain.arch.cucumberdemo.CucumberDemoApplication;
@@ -10,7 +9,6 @@ import com.upchain.arch.cucumberdemo.CucumberDemoApplication;
 @SpringBootTest(classes = CucumberDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration
 @CucumberContextConfiguration
-@AutoConfigureMockMvc
 public class CucumberTestContextConfiguration {
 
     @Before
