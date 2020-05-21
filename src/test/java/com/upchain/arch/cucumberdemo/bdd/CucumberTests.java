62443224a97a893732ad33a64f77fd005841b525
RahulV <rverma@upchain.com>
2020-05-21 05:38:58 -0400

    :chart_with_upwards_trend: Adds Cucumber reports

--- src/test/java/com/upchain/arch/cucumberdemo/bdd/CucumberTests.java
+++ src/test/java/com/upchain/arch/cucumberdemo/bdd/CucumberTests.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+        plugin = {"pretty",
+        "json:target/cucumber-report.json", "html:target/cucumber"})
 public class CucumberTests {
 
 }
