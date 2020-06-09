3cdfe65cb25dd4628671f0a801eba91174166bae
RahulV <rverma@upchain.com>
2020-06-09 13:52:14 -0400

    :art: Updates Get annotations

--- src/main/java/com/upchain/arch/cucumberdemo/controller/HelloController.java
+++ src/main/java/com/upchain/arch/cucumberdemo/controller/HelloController.java
@@ -1,17 +1,18 @@
 package com.upchain.arch.cucumberdemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(method={RequestMethod.GET},value={"/version"})
+    @GetMapping(value={"/version"})
     public String getVersion() {
         return "v0.1";
     }
 
-    @RequestMapping(method={RequestMethod.GET},value={"/hello"})
+    @GetMapping(value={"/hello"})
     public String sayHello() {
         return "Hello from Cucumber!";
     }
